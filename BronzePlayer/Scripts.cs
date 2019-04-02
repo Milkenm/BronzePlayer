e76f8b9f4e9ecc48fa2aef25f64b1a826005ea38
Milkenm <miguel.campos.johnny@gmail.com>
2019-04-02 10:40:48 +0100

    Added the "Not a License" thing to some files.
    You can remove it, don't be afraid, it won't eat you.

--- BronzePlayer/Scripts.cs
+++ BronzePlayer/Scripts.cs
@@ -432,3 +432,27 @@ public class Scripts
         #endregion Stop
     }
 }
+
+
+
+/*
+ *  ╔═══════════════════════════════════════════════════════════════════════════════════════════╗
+ *  ║▓▒░           THIS IS A LICENSE (or not, just something I typed in notepad)             ░▒▓║
+ *   ║▓▒░                                                                                     ░▒▓║
+ *    ║▓▒░                                                                                     ░▒▓║
+ *     ║▓▒░This file has been stolen* from https://github.com/Milkenm/BronzePlayer              ░▒▓║
+ *     ║▓▒░                                                                                     ░▒▓║
+ *    ║▓▒░ This file can be used, because it's a file, and you can share it,                   ░▒▓║
+ *   ║▓▒░    and if you keep this little message, you will make me happy.                     ░▒▓║
+ *  ║▓▒░     Please don't remove it =) It even has this cute map-shaped box and bad english! ░▒▓║
+ *  ║▓▒░   If you received a copy of this file, and can see this message, congrats,          ░▒▓║
+ *  ║▓▒░     the person that gave you this file is a nice human!                             ░▒▓║
+ *   ║▓▒░                                                                                     ░▒▓║
+ *    ║▓▒░                                                                                     ░▒▓║
+ *     ║▓▒░    *jk, this file was not stolen, chill. - or was it?                               ░▒▓║
+ *     ║▓▒░                                                                                     ░▒▓║
+ *     ║▓▒░                                                                                     ░▒▓║
+ *    ║▓▒░                                                          Typed by: Milkenm          ░▒▓║
+ *   ║▓▒░                                                                                     ░▒▓║
+ *  ╚═══════════════════════════════════════════════════════════════════════════════════════════╝
+*/

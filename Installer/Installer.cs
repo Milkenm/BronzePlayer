fce8b5f55b32eadb3b17c436869c23bfc16f29ce
Milkenm <miguel.campos.johnny@gmail.com>
2019-04-02 01:18:47 +0100

    Changed version to 0.2.0.
    I forgot to change the version 🤦

--- Installer/Installer.cs
+++ Installer/Installer.cs
@@ -16,7 +16,7 @@ namespace Installer
     {
         #region Vars
         // # ================================================================================================================================= #
-        string version = "0.1.1";
+        string version = "0.2.0";
         string regPath = @"Software\Milkenm\Bronze Player";
         string arch;
         bool install = false, newerInstalled = false;
